c88a0136d169c44aec3c9079570ede9ec282dfac
Matt Odille <matt.odille@gmail.com>
2013-12-27 12:24:44 -0500

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Modille::Application.routes.draw do
   root 'welcome#index'
-end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
