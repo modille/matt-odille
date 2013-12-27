659f12209982cead580447b93951607c9466913a
Matt Odille <matt.odille@gmail.com>
2013-12-27 12:19:51 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Modille::Application.config.secret_key_base = '72741d7988e31d7aa6cf70437f9ce261c44c5b7084ddef251f3b3d788ce33a9bda216cbad1e37864f4562ef71da9b32b3dd3ec4233040aaddf49897a9817023a'
